57efd363acfaca20c7c8dc0578a49283cdb26d45
Gordon Wrigley <gordon.wrigley@gmail.com>
2020-03-24 21:49:40 +0000

    test duplicates and garbage collection

--- tests/models.py
+++ tests/models.py
@@ -6,9 +6,15 @@ class Friend(models.Model):
     pass
 
 
+class Associate(models.Model):
+    number = models.IntegerField()
+
+
 class Vanilla(models.Model):
     friend = models.ForeignKey(Friend, null=True, on_delete=models.CASCADE)
+    associates = models.ManyToManyField(Associate)
 
 
 class Prefetch(auto_prefetch.Model):
     friend = auto_prefetch.ForeignKey(Friend, null=True, on_delete=models.CASCADE)
+    associates = models.ManyToManyField(Associate)
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,5 @@
+import gc
+
 import pytest
 
 from . import models
@@ -46,8 +48,47 @@ def test_values(django_assert_num_queries, Model, queries):
             print(obj_pk, friend_pk)
 
 
-# duplicate objects
-# garbage collected stuff
+@pytest.mark.parametrize("Model,queries", [(models.Vanilla, 7), (models.Prefetch, 2)])
+@pytest.mark.django_db
+def test_multiples(django_assert_num_queries, Model, queries):
+    friend = models.Friend.objects.create()
+    associates = [models.Associate.objects.create(number=6) for _ in range(2)]
+    for _ in range(3):
+        obj = Model.objects.create(friend=friend)
+        obj.associates.set(associates)
+
+    with django_assert_num_queries(queries):
+        objs = list(Model.objects.filter(associates__number__gt=1))
+        assert len(objs) == 6
+        for obj in objs:
+            print(obj.pk, obj.friend)
+
+
+@pytest.mark.django_db
+def test_garbage_collection():
+    def check_instances(num):
+        gc.collect()
+        objs = [o for o in gc.get_objects() if isinstance(o, models.Prefetch)]
+        assert len(objs) == num
+
+    friend = models.Friend.objects.create()
+    [models.Prefetch.objects.create(friend=friend) for _ in range(3)]
+    del friend
+
+    check_instances(0)
+
+    objs = list(models.Prefetch.objects.all())
+
+    check_instances(3)
+
+    obj = objs[0]
+    del objs
+
+    check_instances(1)
+
+    print(obj.pk, obj.friend)
+
+
 # cascades
 # mixing parts of the system?
 # mixed nulls?
