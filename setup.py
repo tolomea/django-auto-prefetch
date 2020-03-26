2cfed25153702be50b5a1bcc6e25e02c3edacf3b
Gordon Wrigley <gordon.wrigley@gmail.com>
2020-03-26 15:25:10 +0000

    it works on 3.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-auto-prefetch",
-    version="0.0.1",
+    version="0.0.3",
     author="Gordon Wrigley",
     author_email="gordon.wrigley@gmail.com",
     description="git@github.com:tolomea/django-auto-prefetch.git",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.5",
 )
