3303dedfe1ee0787ea340ea890d7ac78b085122e
Adam Johnson <me@adamj.eu>
2020-09-14 09:27:04 +0100

    Move from setup.py to setup.cfg, add pyproject.toml build system declaration (#10)
    This is the more modern way of declaring package metadata. I've added some extra classifiers, fixed the description, and moved the source into `src/`.
    The license metadata was also out of line with the license in LICENSE, so I've updated it to match.

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="django-auto-prefetch",
-    version="0.0.6",
-    author="Gordon Wrigley",
-    author_email="gordon.wrigley@gmail.com",
-    description="git@github.com:tolomea/django-auto-prefetch.git",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/tolomea/django-auto-prefetch",
-    packages=setuptools.find_packages(exclude=["tests"]),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.5",
-)
+setup()
