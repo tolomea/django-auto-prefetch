a0eed446f612b9ba8c908f00399bb511247d5d3e
Adam Johnson <me@adamj.eu>
2021-08-20 11:45:30 +0100

    Add type hints (#85)

--- src/auto_prefetch/__init__.py
+++ src/auto_prefetch/__init__.py
@@ -1,26 +1,50 @@
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Type
 from weakref import WeakValueDictionary
 
 from django.core import checks
 from django.db import models
 from django.db.models.fields import related_descriptors
 
+if TYPE_CHECKING:  # pragma: no cover
 
-class DescriptorMixin:
-    def _field_name(self):
+    class DescriptorBase:
+        field: models.Field
+
+        def is_cached(self, instance: models.Model) -> bool:
+            ...
+
+        def __get__(
+            self,
+            instance: Optional[models.Model],
+            instance_type: Optional[Type[models.Model]] = None,
+        ) -> Any:
+            ...
+
+
+else:
+    DescriptorBase = object
+
+
+class DescriptorMixin(DescriptorBase):
+    def _field_name(self) -> str:
         return self.field.name
 
-    def _is_cached(self, instance):
+    def _is_cached(self, instance: models.Model) -> bool:
         return self.is_cached(instance)
 
-    def _should_prefetch(self, instance):
+    def _should_prefetch(self, instance: Optional[models.Model]) -> bool:
         return (
             instance is not None  # getattr on the class passes None to the descriptor
             and not self._is_cached(instance)  # already loaded
             and len(getattr(instance, "_peers", [])) >= 2  # no peers no prefetch
         )
 
-    def __get__(self, instance, instance_type=None):
-        if self._should_prefetch(instance):
+    def __get__(
+        self,
+        instance: Optional[models.Model],
+        instance_type: Optional[Type[models.Model]] = None,
+    ) -> Any:
+        if instance is not None and self._should_prefetch(instance):
             prefetch = models.query.Prefetch(self._field_name())
             peers = [p for p in instance._peers.values() if not self._is_cached(p)]
             models.query.prefetch_related_objects(peers, prefetch)
@@ -28,7 +52,7 @@ class DescriptorMixin:
 
 
 class ForwardDescriptorMixin(DescriptorMixin):
-    def _should_prefetch(self, instance):
+    def _should_prefetch(self, instance: Optional[models.Model]) -> bool:
         return super()._should_prefetch(
             instance
         ) and None not in self.field.get_local_related_value(
@@ -51,10 +75,10 @@ class ForwardOneToOneDescriptor(
 class ReverseOneToOneDescriptor(
     DescriptorMixin, related_descriptors.ReverseOneToOneDescriptor
 ):
-    def _is_cached(self, instance):
+    def _is_cached(self, instance: models.Model) -> bool:
         return self.related.is_cached(instance)
 
-    def _field_name(self):
+    def _field_name(self) -> str:
         return self.related.get_accessor_name()
 
 
@@ -68,7 +92,7 @@ class OneToOneField(models.OneToOneField):
 
 
 class QuerySet(models.QuerySet):
-    def _fetch_all(self):
+    def _fetch_all(self) -> None:
         set_peers = self._result_cache is None
         super()._fetch_all()
         # ModelIterable tests for query sets returning model instances vs
@@ -90,7 +114,7 @@ class Model(models.Model):
     objects = Manager()
     prefetch_manager = Manager()
 
-    def __getstate__(self):
+    def __getstate__(self) -> Dict[str, Any]:
         # drop the peers info when pickling etc
         res = super().__getstate__()
         if "_peers" not in res:  # pragma: no cover
@@ -101,13 +125,13 @@ class Model(models.Model):
         return res
 
     @classmethod
-    def check(cls, **kwargs):
-        errors = super().check(**kwargs)
+    def check(cls, **kwargs: Any) -> List[checks.Error]:
+        errors: List[checks.Error] = super().check(**kwargs)
         errors.extend(cls._check_meta_inheritance())
         return errors
 
     @classmethod
-    def _check_meta_inheritance(cls):
+    def _check_meta_inheritance(cls) -> List[checks.Error]:
         errors = []
         if not issubclass(cls.Meta, Model.Meta):
             errors.append(
